9ce5ad13097f8344be1e44f37946ae1aba81c599
Kyle-Wisecarver <wiseworks.2022@gmail.com>
2022-04-25 12:49:59 -0500

    Problems two through 4 complete

---
+++ demo.js
@@ -0,0 +1,41 @@
+let animals = [
+{
+        "type": "cat",
+        "legs": 4
+    },
+    {
+        "type": "dog",
+        "legs": 4
+    },
+    {
+        "type": "horse",
+        "legs": 4
+    },
+    {
+        "type": "fish",
+        "legs": 0
+    },
+    {
+        "type": "bird",
+        "legs": 2
+    },
+]
+
+//what am i doing?
+//I want only four legged animals
+//What statement wll be true for the ones I want to keep
+//animal.legs == 4
+//let fourLeggedAnimals = animals.filter(function (animal){
+//    console.log(animal);
+//    if (animal.legs == 4) {
+//        return true;
+//    }
+//    else {
+//        return false;
+//    }
+//});
+//fourLeggedAnimals()
+
+// what am I doing?
+// I want to capitalize all animal types
+//What line of code will do that?
