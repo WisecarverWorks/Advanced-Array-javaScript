cc67b2ca304691848b51672d9547845e3c1f8f65
Kyle-Wisecarver <wiseworks.2022@gmail.com>
2022-04-25 16:09:45 -0500

    Problem 8 complete

--- app.js
+++ app.js
@@ -249,15 +249,31 @@ function newDishNames() {
     return result;
 }
 let cuisineTypeName = newDishNames()
-console.log(cuisineTypeName)
+console.log("Cuisine type then name:  ",cuisineTypeName)
 
 ////8. Create a function that will append the cuisine type to the start of the dish's name. Then, return only the Vegetarian dish objects. So this function should return objects 11-13 with their names changed to "Vegetarian Lasagna", "Vegetarian Falafel", "Vegetarian Chili"
 ////Map, Filter
 //function problemEight() {
-//} 
+//}
+
+function newDishNamesCuisine() {
+    let resultStageOne = dishes.map(function (el) {
+        return el.cuisine + '' + el.name;
+    })
+    let resultStageTwo = resultStageOne.filter(function (element) {
+        if (element.includes('Vegetarian')) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    })
+    return resultStageTwo;
+}
+let newDish = newDishNamesCuisine()
+console.log(newDish)
 
 //BONUS
-
 //5. Create a function that will return dishes whose ingredients array INCLUDES "tomato" OR "cheese".
 //Hint: You do not want to check the array's indexes to find out what the array INCLUDES.
 //Filter
